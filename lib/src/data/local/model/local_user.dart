1dfb4447ee0342817c07ba0619fc952ca5cd3952
Alph4 <julienjennequin59123@gmail.com>
2024-09-16 13:33:27 +0200

    V1 in progress

--- lib/src/data/local/model/local_user.dart
+++ lib/src/data/local/model/local_user.dart
@@ -1,4 +1,4 @@
-
+import 'package:uuid/uuid.dart';
 
 class LocalUser {
   static final LocalUser _instance = LocalUser._internal();
@@ -9,21 +9,15 @@ class LocalUser {
 
   LocalUser._internal();
 
-  String? uid;
-  String? email;
-  String? displayName;
+  String? uid = Uuid().v4();
+  String? email = "test@test.test";
+  String? displayName = "test";
   String? profileImageUrl;
-
 
   void clearUser() {
     uid = null;
     email = null;
     displayName = null;
     profileImageUrl = null;
-
-    
   }
-
-
-
 }
